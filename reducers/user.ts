04cf7f746423425564b174887b8d481980497631
Camille <gryspeerdt.camille@gmail.com>
2023-04-11 18:43:59 +0200

    save before merge

--- reducers/user.ts
+++ reducers/user.ts
@@ -9,7 +9,7 @@ export type UserState = {
       image: string | null;
       age: number | null,
       gender: string | null,
-      userplaces: (string | number |  {} )[];
+      userplaces: [],
     };
   };
 
@@ -22,33 +22,46 @@ const initialState: UserState = {
     image: null,
     age: null,
     gender: null,
-    userplaces:[]},
+    userplaces:null},
 };
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
   reducers: {
+    updateFirstname:(state, action: PayloadAction<string>) => {
+      state.value.firstname = action.payload;
+    },
+    updateUsername:(state, action: PayloadAction<string>) => {
+      state.value.username = action.payload;
+    },
     updateEmail: (state, action: PayloadAction<string>) => {
       state.value.email = action.payload;
     },
+    updateImage:(state, action: PayloadAction<string>) => {
+      state.value.image = action.payload;
+    },
+    updateAge:(state, action: PayloadAction<number>) => {
+      state.value.age = action.payload
+    },
+    updateGender:(state, action: PayloadAction<string>) => {
+      state.value.gender = action.payload;
+    },
+    
+
+
+
     addPlace: (state, action: PayloadAction<string>) => {
       state.value.userplaces.push(action.payload);
     },
-    removePlace: (state, action: PayloadAction<{}>) => {
+    removePlace: (state, action) => {
       state.value.userplaces = state.value.userplaces.filter(e => e.userplaces !== action.payload);
     },
-    importUserPlaces: (state, action:PayloadAction<{}>) => {
+    importUserPlaces: (state, action) => {
       state.value.userplaces.push(action.payload);
     },
-    addImage: (state, action: PayloadAction<string>) => {
-      state.value.image  = action.payload;
-      },
-    updateImage: (state, action: PayloadAction<string>) => {
-      state.value.image  = action.payload;
-      },
   },
 });
 
-export const { updateEmail, addPlace, removePlace, importUserPlaces, addImage, updateImage } = userSlice.actions;
+export const { updateFirstname,updateUsername,updateEmail,updateImage,updateAge,updateGender, addPlace, removePlace, importUserPlaces } = userSlice.actions;
 export default userSlice.reducer;
