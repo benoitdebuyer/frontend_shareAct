29dd6f76fbe91802c5a13a8150275601c1a04949
Triple Patte <triplepatte123@gmail.com>
2023-04-17 18:03:05 +0200

    new screen filter

--- screens/SignInScreen.tsx
+++ screens/SignInScreen.tsx
@@ -35,46 +35,46 @@ export default function HomeScreen({ navigation }) {
   const [hidePassword, setHidePassword] = useState(true);
 
 
-  //   const handleSubmit = () => {
-  // // dispatch pour test
-  // dispatch(updateToken('U4t1K88SoTEvn5wHM08X5ASxfk4WYniw'))
-  // dispatch(updateFirstname('Test'))
-  // dispatch(updateUsername('Test'))
-  // dispatch(updateEmail('Test@Test.Test'))
-  // dispatch(updateAge('20'))
-  // dispatch(updateGender('Autre'))
+    const handleSubmit = () => {
+  // dispatch pour test
+  dispatch(updateToken('U4t1K88SoTEvn5wHM08X5ASxfk4WYniw'))
+  dispatch(updateFirstname('Test'))
+  dispatch(updateUsername('Test'))
+  dispatch(updateEmail('Test@Test.Test'))
+  dispatch(updateAge('20'))
+  dispatch(updateGender('Autre'))
 
-    //   navigation.navigate("TabNavigator", { screen: "Map" });
-    // }
+      navigation.navigate("TabNavigator", { screen: "Map" });
+    }
 
 
-  const handleSubmit = () => {
-    fetch(`${BACKEND_ADDRESS}/users/signin`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ email, password }),
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      if (!data.result) {
-        setEmailError(true);
-      } else {
+//   const handleSubmit = () => {
+//     fetch(`${BACKEND_ADDRESS}/users/signin`, {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({ email, password }),
+//     })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       if (!data.result) {
+//         setEmailError(true);
+//       } else {
        
        
-        dispatch(updateFirstname(data.username));
-        dispatch(updateUsername(data.firstname));
-        dispatch(updateToken(data.token));
-        dispatch(updateEmail(email));
-        dispatch(updateGender(data.gender));
-        dispatch(updateDatebirth(data.age));
+//         dispatch(updateFirstname(data.username));
+//         dispatch(updateUsername(data.firstname));
+//         dispatch(updateToken(data.token));
+//         dispatch(updateEmail(email));
+//         dispatch(updateGender(data.gender));
+//         dispatch(updateDatebirth(data.age));
 
-        // appel de la fonction calculateAge qui calcule l'age par rapport a la date de naissance.
-        const agecalculated = calculateAge(new Date(data.age)); 
-        dispatch(updateAge(agecalculated))
-        navigation.navigate("TabNavigator", { screen: "Map" });
-      }
-    })
-}
+//         // appel de la fonction calculateAge qui calcule l'age par rapport a la date de naissance.
+//         const agecalculated = calculateAge(new Date(data.age)); 
+//         dispatch(updateAge(agecalculated))
+//         navigation.navigate("TabNavigator", { screen: "Map" });
+//       }
+//     })
+// }
 
 
 const calculateAge = (dateOfBirth) => {
