8ecc97b05b2c14c272a390dc7e6828a5933a0c6d
Camille <gryspeerdt.camille@gmail.com>
2023-05-02 19:42:22 +0200

    ajout d un test de signUp

--- frontShareAct.test.js
+++ frontShareAct.test.js
@@ -1,9 +1,34 @@
 import {  exportedForTesting } from './screens/SignInScreen';
 const { calculateAge } = exportedForTesting;
+const API_URL = 'https://shareact-backend.vercel.app';
 
 describe('Test sur la fonction calculateAge de SignInScreen.tsx : ', () => {
   it('doit calculer l âge correctement', () => {
     const testDate = new Date('1990-01-01');
     expect(calculateAge(testDate)).toBe(33);
   });
+});
+
+test('Test inscription avec la route signup', async () => {
+  const userData = {
+    firstname: 'Camille',
+    username: 'Trinity0077',
+    email: 'kam@trinity0077.com',
+    password: '1234supercode',
+    age: '1986-02-02',
+    gender: 'Femme'
+  };
+
+  const response = await fetch(`${API_URL}/users/signup`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userData)
+  });
+
+  const data = await response.json();
+  expect(response.status).toEqual(200);
+  expect(data.result).toBe(true);
+  expect(data.token).toBeDefined();
 });
